5ebc30bb0f332aba8a796292f15d07c7ff505e34
Tomi <63859937+tomicancheff@users.noreply.github.com>
2022-11-02 16:41:55 -0300

    Added Axe melee Attack, enemy reaction

---
+++ Assets/CollisionDetection.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Main.C_Scripts.Tomi;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    [SerializeField] private PlayerAttack weapon;
+    [SerializeField] private GameObject hitParticle;
+    private static readonly int Hit = Animator.StringToHash("Hit");
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && weapon.isAttacking)
+        {
+            other.GetComponent<Animator>().SetTrigger(Hit);
+            Debug.Log(other.name);
+            Instantiate(hitParticle, new Vector3(other.transform.position.x, transform.position.y,
+                other.transform.position.z), other.transform.rotation);
+        }
+    }
+}
--- Assets/_Main/C#Scripts/Tomi/PlayerAttack.cs
+++ Assets/_Main/C#Scripts/Tomi/PlayerAttack.cs
@@ -7,20 +7,24 @@ namespace _Main.C_Scripts.Tomi
     public class PlayerAttack : MonoBehaviour
     {
         public GameObject hand;
-     
         
         //WEAPONS HOLDER
-        // public GameObject warHammer;
-        // public GameObject longSword;
+         // public GameObject warHammer;
+         // public GameObject axe;
+         // public GameObject longSword;
         // public GameObject mace;
         // public GameObject spear;
         // public GameObject crossbow;
+        
+        //WEAPON AUDIO
+        [SerializeField] private AudioClip axeAttackSound;
+        
         
         //WEAPON ATTACK  TRIGGER
         public bool canAttack = true;
         public float attackCoolDown = 1.0f;
         private static readonly int Attack = Animator.StringToHash(("Attack"));
-
+        public bool isAttacking = false;
 
         private void Update()
         {
@@ -29,67 +33,85 @@ namespace _Main.C_Scripts.Tomi
                 if (canAttack)
                 {
                     AxeAttack();
-                    HammerAttack();
-                        SwordAttack();
-                        MaceAttack();
-                        SpearAttack();
+                    // HammerAttack();
+                    //     SwordAttack();
+                    //     MaceAttack();
+                    //     SpearAttack();
                 }
             }
         }
+        
         //DIFFERENT WEAPON ATTACKS---------------------------------------
 
         // ReSharper disable Unity.PerformanceAnalysis
         private void AxeAttack()
         {
+            isAttacking = true;
             canAttack = false;
             Animator anim = hand.GetComponent<Animator>();
             anim.SetTrigger(Attack);
+            AudioSource axeAttack= GetComponent<AudioSource>();
+            axeAttack.PlayOneShot(axeAttackSound);
             StartCoroutine(ResetAttackCoolDown());
 
         }
 
         // ReSharper disable Unity.PerformanceAnalysis
-        private void HammerAttack()
-        {
-            canAttack = false;
-            Animator anim = hand.GetComponent<Animator>();
-            anim.SetTrigger(Attack);
-            StartCoroutine(ResetAttackCoolDown());
-        }
+       
+        // private void HammerAttack()
+        // {
+        //     canAttack = false;
+        //     Animator anim = hand.GetComponent<Animator>();
+        //     anim.SetTrigger(Attack);
+        //     StartCoroutine(ResetAttackCoolDown());
+        // }
         
         // ReSharper disable Unity.PerformanceAnalysis
-        private void SwordAttack()
-        {
-            canAttack = false;
-            Animator anim = hand.GetComponent<Animator>();
-            anim.SetTrigger(Attack);
-            StartCoroutine(ResetAttackCoolDown());
-        }
+        
+        
+        // private void SwordAttack()
+        // {
+        //     canAttack = false;
+        //     Animator anim = hand.GetComponent<Animator>();
+        //     anim.SetTrigger(Attack);
+        //     StartCoroutine(ResetAttackCoolDown());
+        // }
         
         // ReSharper disable Unity.PerformanceAnalysis
-        private void MaceAttack()
-        {
-            canAttack = false;
-            Animator anim = hand.GetComponent<Animator>();
-            anim.SetTrigger(Attack);
-            StartCoroutine(ResetAttackCoolDown());
-        }
+        
+        
+        // private void MaceAttack()
+        // {
+        //     canAttack = false;
+        //     Animator anim = hand.GetComponent<Animator>();
+        //     anim.SetTrigger(Attack);
+        //     StartCoroutine(ResetAttackCoolDown());
+        // }
         
         // ReSharper disable Unity.PerformanceAnalysis
-        private void SpearAttack()
-        {
-            canAttack = false;
-            Animator anim = hand.GetComponent<Animator>();
-            anim.SetTrigger(Attack);
-            StartCoroutine(ResetAttackCoolDown());
-        }
+        
+        
+        // private void SpearAttack()
+        // {
+        //     canAttack = false;
+        //     Animator anim = hand.GetComponent<Animator>();
+        //     anim.SetTrigger(Attack);
+        //     StartCoroutine(ResetAttackCoolDown());
+        // }
 
         //COROUTINE FOR RESET ATTACK TRIGGER
         IEnumerator ResetAttackCoolDown()
         {
+            StartCoroutine(ResetAttackBool());
             yield return new WaitForSeconds(attackCoolDown);
             canAttack = true;
         }
-        
+
+
+        IEnumerator ResetAttackBool()
+        {
+            yield return new WaitForSeconds(1.0f);
+            isAttacking = false;
+        }
     }
 }
