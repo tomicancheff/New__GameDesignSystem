4ecaf48bd3ad4aa4cffa507e7c5c847881b364f6
Tomi <63859937+tomicancheff@users.noreply.github.com>
2022-11-02 15:33:57 -0300

    Added new PlayerAttack Script

--- Assets/_Main/C#Scripts/Tomi/PlayerAttack.cs
+++ Assets/_Main/C#Scripts/Tomi/PlayerAttack.cs
@@ -1,18 +1,95 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
+namespace _Main.C_Scripts.Tomi
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerAttack : MonoBehaviour
     {
+        public GameObject hand;
+     
         
-    }
+        //WEAPONS HOLDER
+        // public GameObject warHammer;
+        // public GameObject longSword;
+        // public GameObject mace;
+        // public GameObject spear;
+        // public GameObject crossbow;
+        
+        //WEAPON ATTACK  TRIGGER
+        public bool canAttack = true;
+        public float attackCoolDown = 1.0f;
+        private static readonly int Attack = Animator.StringToHash(("Attack"));
 
-    // Update is called once per frame
-    void Update()
-    {
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (canAttack)
+                {
+                    AxeAttack();
+                    HammerAttack();
+                        SwordAttack();
+                        MaceAttack();
+                        SpearAttack();
+                }
+            }
+        }
+        //DIFFERENT WEAPON ATTACKS---------------------------------------
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void AxeAttack()
+        {
+            canAttack = false;
+            Animator anim = hand.GetComponent<Animator>();
+            anim.SetTrigger(Attack);
+            StartCoroutine(ResetAttackCoolDown());
+
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void HammerAttack()
+        {
+            canAttack = false;
+            Animator anim = hand.GetComponent<Animator>();
+            anim.SetTrigger(Attack);
+            StartCoroutine(ResetAttackCoolDown());
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void SwordAttack()
+        {
+            canAttack = false;
+            Animator anim = hand.GetComponent<Animator>();
+            anim.SetTrigger(Attack);
+            StartCoroutine(ResetAttackCoolDown());
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void MaceAttack()
+        {
+            canAttack = false;
+            Animator anim = hand.GetComponent<Animator>();
+            anim.SetTrigger(Attack);
+            StartCoroutine(ResetAttackCoolDown());
+        }
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void SpearAttack()
+        {
+            canAttack = false;
+            Animator anim = hand.GetComponent<Animator>();
+            anim.SetTrigger(Attack);
+            StartCoroutine(ResetAttackCoolDown());
+        }
+
+        //COROUTINE FOR RESET ATTACK TRIGGER
+        IEnumerator ResetAttackCoolDown()
+        {
+            yield return new WaitForSeconds(attackCoolDown);
+            canAttack = true;
+        }
         
     }
 }
